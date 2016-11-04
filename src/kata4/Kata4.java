86f3a3306891c9444356a0109a610b42aa2c06ee
usuario <usuario@10.22.143.44>
2016-11-04 09:55:17 +0000

    Kata4  v3

---
+++ src/kata4/Kata4.java
@@ -0,0 +1,18 @@
+package kata4;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Kata4 {
+
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        String fileName = "emailsfilev1.txt";
+        ArrayList<String> mailList = MailListReader.read(fileName);
+        Histogram<String> histogram = MailHistogramBuilder.build(mailList);
+        HistogramDisplay histoDisplay = new HistogramDisplay(histogram);
+        histoDisplay.execute();
+    }
+    
+}
