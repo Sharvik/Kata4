48cf79787bb21c14cc8f3c9fb958f6433532ecfa
usuario <usuario@10.22.143.44>
2016-11-04 09:57:23 +0000

    Kata4  v1

--- src/kata4/Kata4.java
+++ src/kata4/Kata4.java
@@ -7,12 +7,13 @@ import java.util.ArrayList;
 
 public class Kata4 {
 
-    public static void main(String[] args) throws FileNotFoundException, IOException {
-        String fileName = "emailsfilev1.txt";
-        ArrayList<String> mailList = MailListReader.read(fileName);
-        Histogram<String> histogram = MailHistogramBuilder.build(mailList);
-        HistogramDisplay histoDisplay = new HistogramDisplay(histogram);
-        histoDisplay.execute();
+    public static void main(String[] args) {
+        
+        File file = new File("c:\\");
+        String[] names = file.list();
+        for (String name : names) {
+            System.out.println(name);
+        }
     }
     
 }
